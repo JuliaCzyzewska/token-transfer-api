283bf401a955b3925c70448ec0cb291663fac722
Julia Czyzewska <julaczyzewska28@gmail.com>
2025-07-09 15:52:38 +0200

    Add local connection to postgres DB

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
@@ -13,5 +16,31 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	// Build DB connection string
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+	)
+	fmt.Println(connStr)
+
+	// Open DB connection
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Error connecting to DB:", err)
+	}
+
+	// Close connection when main() finishes
+	defer db.Close()
+
+	// Check if DB is reachable
+	if err := db.Ping(); err != nil {
+		log.Fatal("Ping failed:", err)
+	}
+
+	fmt.Println("Connected to DB.")
 
 }
